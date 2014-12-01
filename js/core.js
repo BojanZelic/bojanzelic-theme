d1212d5b4791c7f975badd7faed737b8e09cb84e
Bojan Zelic <bnzelic@gmail.com>
2014-11-30 17:12:31 -0700

    Set Header Image to featured Wordpress Image, add FX to header

--- js/core.js
+++ js/core.js
@@ -2,6 +2,18 @@ jQuery(document).ready(function($) {
 
   $.material.init();
 
+  //fade header on scroll
+  (function(){
+    var h1 = $('.main-header-image').find('h1');
+    changeScroll();
+    $(window).scroll(changeScroll);
+
+    function changeScroll(){
+      var scrollVar = $(window).scrollTop();
+      h1.css({'margin-top': .7*scrollVar, 'opacity':( 150-scrollVar )/100 });
+    }
+  })();
+
   $("#menu-toggle").click(function(e) {
     e.preventDefault();
     $("#wrapper").toggleClass("toggled");
